b6ce40f484bd17c5fa6d6f200119a49dc54de70a
elsaosmani <eosmani@ufl.edu>
2023-11-19 15:43:57 -0500

    Prototype State

--- sockets.py
+++ sockets.py
@@ -5,9 +5,16 @@ import websockets
 import ssl
 import pathlib
 import time
+import requests
 
 ID = "12345678"
-server_url = "wss://192.168.137.1:8000/ws"
+auto_id = ""
+token = ""
+headers = ""
+connected = False
+server_url = "wss://10.138.224.13:8000/ws"
+http_url = "https://10.138.224.13:8000/authorize"
+
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 #localhost_pem1 = pathlib.Path(__file__ + "/keys").with_name("cert.pem")
@@ -33,6 +40,7 @@ ssl_context.check_hostname = False  # Disable hostname verification
 #[ green, red, yellow, blue]
 BUTTON_PINS = [4, 27, 10, 9]
 LED_PINS = [15, 14, 17, 18]
+BUTTONS_NOTES = [48, 50, 52, 53]
 
 # Pairing each button with an LED
 button_led_pairs = {4: 15, 27: 14, 10: 17, 9: 18}
@@ -42,6 +50,9 @@ start_time = {pin: 0 for pin in BUTTON_PINS}
 time_duration = {pin: 0 for pin in BUTTON_PINS}
 #keeping track of which button state was changed, 0 -> not changed, 1 -> changed (rising or falling edge detected)
 button_state = {pin: 0 for pin in BUTTON_PINS}
+
+button_note = {48: 4, 50: 27, 52: 10, 53: 9}
+note_button = {4: 48, 27: 50, 10: 52, 9: 53}
 
 #########################################################################################################
 # Interrupt handler function, marks the pin that the rising or falling edge was detected from
@@ -85,6 +96,7 @@ def light_up_led(pin, state):
 #########################################################################################################
 # Function to handle the received WebSocket message
 async def receive_messages(websocket):
+    global auto_id, token
     try:
         button_message_sent = {pin: False for pin in BUTTON_PINS}
 
@@ -93,13 +105,14 @@ async def receive_messages(websocket):
             if raw_message:
                 message = json.loads(raw_message)
                 #print("Received:", message)
-
-                pin = message.get("pin")
+                
+                note = message.get("note")
+                pin = button_note[int(note)]
                 state = message.get("state")
                 light_up_led(int(pin), int(state))
 
                 if int(state) == 1 and not button_message_sent[int(pin)]:
-                    message = {"id": ID, "pin": pin, "state": "1", "start_time": start_time[int(pin)], "duration": time_duration[int(pin)]}
+                    message = {"token": token, "note": note, "state": "1", "start_time": start_time[int(pin)], "duration": time_duration[int(pin)]}
                     print(message)
                     await websocket.send(json.dumps(message))
                     start_time[int(pin)] = 0
@@ -116,28 +129,44 @@ async def receive_messages(websocket):
 #########################################################################################################
 # main loop
 async def main():
-    global start_time, time_duration
+    global start_time, time_duration, connected, headers, token
     setup_gpio()
-    async with websockets.connect(server_url, ssl=ssl_context) as websocket:
-        print("Connected to Server")
-        # Create a task to receive messages
-        recv_task = asyncio.ensure_future(receive_messages(websocket))
 
-        while True:
-            # Check the state of the push button, if changed (=1) handle it
-            for button_pin, led_pin in button_led_pairs.items():
-                if button_state[button_pin] == 1:
-                    if GPIO.input(button_pin) == GPIO.LOW:
-                        # Button is pressed, send a message to the client
-                        message = {"id": ID, "pin": str(button_pin), "state": "0", "start_time": "-1", "duration": "-1"}
-                        await websocket.send(json.dumps(message))
-                    elif GPIO.input(button_pin) == GPIO.HIGH:
-                        # Button is released, send a message to the client
-                        message = {"id": ID, "pin": str(button_pin), "state": "1", "start_time": "-1", "duration": "-1"}
-                        print(message)
-                        await websocket.send(json.dumps(message))
-                    button_state[button_pin] = 0
-            await asyncio.sleep(0.05)  # Add a small delay to avoid busy-wait
+    # Sending HTTP request to authorize websocket connection
+    message = {'hardwareId': ID}
+    response = requests.get(http_url, params = message, verify=False)
+
+    #if not verified
+    if response.status_code != 200:
+        print("No authorization")
+    #if verified, save token and establish websocket connection
+    else:
+        json_object_returned = response.json()
+        print(json_object_returned)
+        token = json_object_returned.get("token")
+        headers = [('Authorization', 'Bearer ' + token)]
+        print(headers)
+        print("I got the token: ", token)
+        async with websockets.connect(server_url, ssl=ssl_context, extra_headers = headers) as websocket:
+            print("Connected to Server")
+            # Create a task to receive messages
+            recv_task = asyncio.ensure_future(receive_messages(websocket))
+
+            while True:
+                # Check the state of the push button, if changed (=1) handle it
+                for button_pin, led_pin in button_led_pairs.items():
+                    if button_state[button_pin] == 1:
+                        if GPIO.input(button_pin) == GPIO.LOW:
+                            # Button is pressed, send a message to the client
+                            message = {"token": token, "note": str(note_button[button_pin]), "state": "0", "start_time": "-1", "duration": "-1"}
+                            await websocket.send(json.dumps(message))
+                        elif GPIO.input(button_pin) == GPIO.HIGH:
+                            # Button is released, send a message to the client
+                            message = {"token": token, "note": str(note_button[button_pin]), "state": "1", "start_time": "-1", "duration": "-1"}
+                            print(message)
+                            await websocket.send(json.dumps(message))
+                        button_state[button_pin] = 0
+                await asyncio.sleep(0.05)  # Add a small delay to avoid busy-wait
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
